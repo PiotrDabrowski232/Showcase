03a3a81b788ac501f2239615a32b6e0ebfa83ad2
piecz <piecz@DESKTOP-D63PQJ6>
2023-04-02 21:16:13 +0200

    ss

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -18,6 +18,7 @@
       <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
       <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
       <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+      <li>asdas</li>
     </ul>
     <h3>Ecosystem</h3>
     <ul>
